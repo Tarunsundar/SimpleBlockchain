8055af73fa39e5098be0f919cf4c8e6446f46fa1
Tarunsundar <47869186+Tarunsundar@users.noreply.github.com>
2022-06-11 13:22:06 +0100

    Add files via upload

--- SimpleBlockChain.hs
+++ SimpleBlockChain.hs
@@ -12,42 +12,46 @@ type PreviousHash = String
 type OverAllDetails = (Details, CurrentHash, PreviousHash) 
 type SampleBlockChain = [OverAllDetails]
 
+fstOfTriple :: SenderName -> OverAllDetails -> OverAllDetails
 fstOfTriple x (_, _, _) = (x, _, _) -- this is to set the first element from tuple,
 
+sndOfTriple :: RecieverName -> OverAllDetails -> OverAllDetails
 sndOfTriple y (_, _, _) = (_, y, _) -- this is to set the second element from tuple, current has
 
+gsndOfTriple :: OverAllDetails -> RecieverName
 gsndOfTriple (_, y, _) = y  -- this is to get the second element from tuple, current has
 
+trdOfTriple :: Amount-> OverAllDetails -> OverAllDetails
 trdOfTriple z (_, _, _) = (_, _, z) -- this is to set the third element from tuple, previoushash
 
-emptyBlockchain :: Details-> OverAllDetails -> SampleBlockChain -> SampleBlockChain
-emptyBlockchain details oDetails blockChain = 
-    fstOfTriple (details, oDetail)
-    sndOfTriple hash details oDetail
-    trdOfTriple(show 0, oDetail)
-    blockChain ++ oDetail
+emptyBlockChain :: Details-> OverAllDetails -> SampleBlockChain -> SampleBlockChain
+emptyBlockChain details oDetails blockChain = 
+    fstOfTriple (details, oDetails)
+    sndOfTriple (hash etails, oDetails)
+    trdOfTriple(show 0, oDetails)
+    blockChain ++ oDetails
 
-nonEmptyBlockchain :: Details -> OverAllDetails -> SampleBlockChain -> SampleBlockChain
-nonEmptyBlockChain details oDetails blockchain = 
-    fstOfTriple (details, oDetail)
-    sndOfTriple hash details oDetail
-    trdOfTriple(gsndOfTriple(prevElement), oDetail)
-    blockChain ++ oDetail
+nonEmptyBlockChain :: Details -> OverAllDetails -> SampleBlockChain -> SampleBlockChain
+nonEmptyBlockChain details oDetails blockChain = 
+    fstOfTriple (details, oDetails)
+    sndOfTriple (hash details, oDetails)
+    trdOfTriple(gsndOfTriple(oDetails), oDetails)
+    blockChain ++ oDetails
 
-generateTransactionDetails :: senderName-> RecieverName -> Amount -> Details 
+generateTransactionDetails :: SenderName-> RecieverName -> Amount -> Details 
 --this method generates transaction details as a string
 generateTransactionDetails sName rName amount = 
     sName ++ rName ++ show amount
 
 insertTransaction :: Details-> OverAllDetails -> SampleBlockChain-> SampleBlockChain
-insertTransaction details oDetail blockChain=  
+insertTransaction details oDetails blockChain=  
     if(length blockChain == 0) --genisis block
-        then emptyBlockchain(details, oDetails, blockChain)
+        then emptyBlockChain(details, oDetails, blockChain)
     else  
-        nonEmptyBlockchain(details, oDetails, blockChain)
+        nonEmptyBlockChain(details, oDetails, blockChain)
 
-printBlockChain :: SampleBlockChain -> IO()
-printBlockChain sBChain = sequence putStrnLn(sBChain)
+printBlockChain :: String -> IO()
+printBlockChain sBChain = putStrLn(sBChain)
  
 main = do 
     let oDetails = OverAllDetails
